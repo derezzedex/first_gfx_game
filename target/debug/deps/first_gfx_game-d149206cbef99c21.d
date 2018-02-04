35b5271afb16cad650f4f9a853db7476ebbd3ac6
derezzedex <richard.custodio@hotmail.com>
2018-02-04 21:24:51 -0200

    Removed the target/debug

--- target/debug/deps/first_gfx_game-d149206cbef99c21.d
+++
@@ -1,7 +0,0 @@
-/home/sadmachine/github/first_gfx_game/target/debug/deps/first_gfx_game-d149206cbef99c21: /home/sadmachine/github/first_gfx_game/src/main.rs /home/sadmachine/github/first_gfx_game/shaders/cube_150.glslv /home/sadmachine/github/first_gfx_game/shaders/cube_150.glslf
-
-/home/sadmachine/github/first_gfx_game/target/debug/deps/first_gfx_game-d149206cbef99c21.d: /home/sadmachine/github/first_gfx_game/src/main.rs /home/sadmachine/github/first_gfx_game/shaders/cube_150.glslv /home/sadmachine/github/first_gfx_game/shaders/cube_150.glslf
-
-/home/sadmachine/github/first_gfx_game/src/main.rs:
-/home/sadmachine/github/first_gfx_game/shaders/cube_150.glslv:
-/home/sadmachine/github/first_gfx_game/shaders/cube_150.glslf:
